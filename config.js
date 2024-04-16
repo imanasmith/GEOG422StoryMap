3966dcad5fcfd502ead1d9867ea25425ce85210f
imanasmith <160679562+imanasmith@users.noreply.github.com>
2024-04-16 16:57:45 -0400

    Initial commit

---
+++ config.js
@@ -0,0 +1,116 @@
+var config = {
+    style: 'mapbox://styles/alanacg/clrjmsn2r000b01p58f1p9cne',
+    accessToken: 'pk.eyJ1IjoiYWxhbmFjZyIsImEiOiJjbHI2dTdmaTgyYmJkMmtteWg4aTFyYjd0In0.4egkQvHeJNw_VNBgcSEZmg',
+    showMarkers: true,
+    markerColor: '#3FB1CE',
+    //projection: 'equirectangular',
+    //Read more about available projections here
+    //https://docs.mapbox.com/mapbox-gl-js/example/projections/
+    inset: false,
+    theme: 'dark',
+    use3dTerrain: false, //set true for enabling 3D maps.
+    auto: false,
+    title: 'The Title Text of this Story',
+    subtitle: 'A descriptive and interesting subtitle to draw in the reader',
+    byline: 'By a Digital Storyteller',
+    footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
+    chapters: [
+        {
+            id: 'slug-style-id',
+            alignment: 'left', //options are left, right, center, full
+            hidden: false, //can hide panel with this
+            title: 'Display Title',
+            image: 'images/source.png',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            //text for description is html- can rely on that for extra formatting
+            location: {
+                center: [-122.418398, 37.759483],
+                zoom: 8.5,    //how close/far you are from map content
+                pitch: 60,    //angle from azimuth to view the map at (0 is straight on)
+                bearing: 0    //offset from 0°N map is viewed from
+            },
+            mapAnimation: 'flyTo',    //more options in mapbox/storytelling repo
+                // flyTo additional controls-
+                // These options control the flight curve, making it move
+                // slowly and zoom out almost completely before starting
+                // to pan.
+                //speed: 2, // make the flying slow
+                //curve: 1, // change the speed at which it zooms out
+            rotateAnimation: false,
+            callback: '', //add additional js function for this key
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+        {
+            id: 'second-identifier', //need unique id for each chapter
+            alignment: 'right',
+            hidden: false,
+            title: 'Second Title',
+            image: 'images/target.svg',
+            description: 'Copy these sections to add to your story.',
+            location: {
+                center: [-77.020636, 38.886900],
+                zoom: 8.5,
+                pitch: 60,
+                bearing: -43.2,
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'third-identifier',
+            alignment: 'left',
+            hidden: false,
+            title: 'Third Title',
+            image: 'images/source.png',
+            description: 'Copy these sections to add to your story.',
+            location: {
+                center: [6.15116, 46.20595],
+                zoom: 12.52,
+                pitch: 8.01,
+                bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'fourth-chapter',
+            alignment: 'full',
+            hidden: false,
+            title: 'Third Title',
+            image: 'images/source.png',
+            description: 'Copy these sections to add to your story.',
+            location: {
+                center: [-58.54195, -34.71600],
+                zoom: 4,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        }
+    //Add any new chapters above this line
+        
+    //Do not edit the two brackets below this line
+    ]
+};
